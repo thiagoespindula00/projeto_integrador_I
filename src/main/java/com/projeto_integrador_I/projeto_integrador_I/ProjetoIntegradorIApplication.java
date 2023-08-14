138243b653b0b17059c376296952bf07e0e3dcea
= <thiagoespindula00@gmail.com>
2023-08-14 19:33:52 -0300

    Initial commit

---
+++ src/main/java/com/projeto_integrador_I/projeto_integrador_I/ProjetoIntegradorIApplication.java
@@ -0,0 +1,13 @@
+package com.projeto_integrador_I.projeto_integrador_I;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoIntegradorIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoIntegradorIApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto_integrador_I/projeto_integrador_I/ProjetoIntegradorIApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto_integrador_I.projeto_integrador_I;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoIntegradorIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
